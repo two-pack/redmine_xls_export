89718c580243cb26694cc2ffa5d02ee52b663b67
Tatsuya Saito <twopackas@gmail.com>
2014-05-07 10:28:30 +0900

    Adds tests default values on Configure page. refs #53

--- test/integration/configure_page_test.rb
+++ test/integration/configure_page_test.rb
@@ -36,12 +36,42 @@ class ConfigurePageTest < ActionController::IntegrationTest
 
   def setup
     login_with_admin
+    visit '/settings/plugin/redmine_xls_export'
+    assert_not_nil page
   end
 
   def test_to_show_configure_page_by_administrator
-    visit '/settings/plugin/redmine_xls_export'
-    assert_not_nil page
     assert page.has_css?('h2', text: ' » Issues XLS export')
+  end
+
+  def test_to_set_default_values_of_columns_options
+    assert_equal page.has_checked_field?('settings_relations'), true
+    assert_equal page.has_checked_field?('settings_watchers'), true
+    assert_equal page.has_checked_field?('settings_journal'), false
+    assert_equal page.has_checked_field?('settings_attachments'), false
+  end
+
+  def test_to_set_default_values_of_export_options
+    assert_equal page.has_checked_field?('settings_query_columns_only'), false
+    assert_equal page.has_checked_field?('settings_group'), false
+    assert_equal page.has_checked_field?('settings_generate_name'), true
+  end
+
+  def test_to_set_default_values_of_extra_options
+    assert_equal page.has_checked_field?('settings_export_attached'), false
+    assert_equal page.has_checked_field?('settings_separate_journals'), false
+  end
+
+  def test_to_set_default_values_of_date_format_options
+    assert page.has_field?('settings_created_format', :with => 'dd.mm.yyyy hh:mm:ss')
+    assert page.has_field?('settings_updated_format', :with => 'dd.mm.yyyy hh:mm:ss')
+    assert page.has_field?('settings_start_date_format', :with => 'dd.mm.yyyy')
+    assert page.has_field?('settings_due_date_format', :with => 'dd.mm.yyyy')
+  end
+
+  def test_to_set_default_values_of_other_options
+    assert page.has_field?('settings_issues_limit', :with => '0')
+    assert page.has_field?('settings_export_name', :with => 'issues_export')
   end
 
   def teardown
