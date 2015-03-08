d0249c227b89bf67cfc4d414f1105204d0695f76
Tatsuya Saito <twopackas@gmail.com>
2015-03-08 10:49:49 +0900

    Adds 3.0.0 to travis.xml and changes 2.6.1 to 2.6.2. refs #86

--- .travis-init.sh
+++ .travis-init.sh
@@ -20,7 +20,7 @@ case $REDMINE_VERSION in
           export MIGRATE_PLUGINS=db:migrate_plugins
           export REDMINE_TARBALL=https://github.com/redmine/redmine/archive/$REDMINE_VERSION.tar.gz
           ;;
-  2.*)  export PATH_TO_PLUGINS=./plugins # for redmine 2.0
+  2.* | 3.*)  export PATH_TO_PLUGINS=./plugins # for redmine 2.x and 3.x
           export GENERATE_SECRET=generate_secret_token
           export MIGRATE_PLUGINS=redmine:plugins:migrate
           export REDMINE_TARBALL=https://github.com/redmine/redmine/archive/$REDMINE_VERSION.tar.gz
